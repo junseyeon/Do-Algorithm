a07ecb31718fe13800b11cef99760ddce65af8c4
junseyeon <s2017s18@e-mirim.hs.kr>
2023-10-26 23:46:12 +0900

    🤡 브루트 포스2

--- bruteforce1.py
+++ bruteforce1.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 ### 666 이 들어간 숫자
 '''
 n = int(input())
@@ -27,12 +29,8 @@ for i in list(combinations(cards, 3)):
 print(my_max) """
 
 
-#### 365 문제 
-
-# 3/6/9가 숫자n 에 몇번이 들어가는지 !
-# n을 각각 숫자가 3/6/9 인지 확인 하는 것보다 훨씬 반복횟수 낮아짐...
-## => 주어진 조건이 있을 때 반대로 생각해보기... 
-import sys
+#### 365 문제  
+""" import sys
 input = sys.stdin.readline
 N = int(input())
 clap=0
@@ -44,15 +42,63 @@ for i in range(1,N+1):
             clap+=1
         if '9' in j:
             clap+=1
-print(clap)
+print(clap) """
 # 시간초과...
 
+# 3/6/9가 숫자n 에 몇번이 들어가는지로 카운트 
+# n을 각각 숫자가 3/6/9 인지 확인 하는 것보다 훨씬 반복횟수 낮아짐...
+## => 주어진 조건이 있을 때 반대로 생각해보기.!
 # str(i) 를 for문 안에서 수행하지 않기!!!
 
-N = int(input())
+""" N = int(input())
 check = '369'
 re = 0
 for i in range(1, N+1):
     for c in check:
         re += str(i).count(c)
-print(re)
+print(re) """
+
+# N=집 수, K=대피소 수 
+# 1.가능한 대피소 조합 만들기
+# 2.조합 중 max가 가장 적은 거리값 출력 
+""" from itertools import combinations
+N, K = map(int,input().split())
+x = [0] * N
+y = [0] * N
+for i in range(N):
+    x[i], y[i] = map(int,input().split())
+
+#1)
+comb = list(combinations(range(N),K))
+print(comb) """
+
+# 무조건 대피소가 집 위치;;;;;
+
+from itertools import combinations
+
+def mii():
+    return map(int, input().split())
+
+N, K = mii()
+INF = float("INF")
+x = [0] * N
+y = [0] * N
+for i in range(N):
+    x[i], y[i] = mii()
+
+def max_dist(comb):
+    rst = 0
+    for i in range(N):
+        min_dist = INF
+        for c in comb:
+            dist = abs(x[i] - x[c]) + abs(y[i] - y[c])
+        min_dist = min(min_dist, dist)
+        rst = max(rst, min_dist)
+    return rst
+
+ans = INF
+for comb in combinations(range(N), K):
+    ans = min(ans, max_dist(comb))
+
+print(ans)
+
