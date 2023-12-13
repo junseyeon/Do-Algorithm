46d6588565fb73775aa84b5ff57699432079ca32
junseyeon <s2017s18@e-mirim.hs.kr>
2023-12-13 17:38:40 +0900

    GCD합_GCD정의

--- 9613_GCD합.py
+++ 9613_GCD합.py
@@ -1,7 +1,12 @@
 #백준 : https://www.acmicpc.net/problem/9613
 # 각 테스트 케이스마다 가능한 모든 쌍의 GCD의 합을 출력한다.
 import math
-N  = int(input())
+
+def GCD(a,b):
+    if b==0: return a
+    return GCD(b, a%b)
+
+N = int(input())
 arr =[]
 for _ in range(N):
     arr.append(list(input().split()[1:]))
@@ -9,5 +14,5 @@ for i in range(N):
     hap = 0
     for j in range(len(arr[i])):
         for k in arr[i][j+1:]:
-            hap += math.gcd(int(arr[i][j]), int(k) )
+            hap += GCD(int(arr[i][j]), int(k))
     print(hap)
