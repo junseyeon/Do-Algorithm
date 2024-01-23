1552df73bd92296316096053f1446b8d6d31ae47
junseyeon <s2017s18@e-mirim.hs.kr>
2024-01-23 15:13:10 +0900

    🥈 배열합치기

--- 투 포인터/배열 합치기.py
+++ 투 포인터/배열 합치기.py
@@ -1,4 +1,7 @@
 # https://www.acmicpc.net/problem/11728
+'''
+두 배열을 비교해서 작으면 인덱스 이동
+'''
 
 import sys
 sys.stdin = open('../input.txt')
@@ -7,8 +10,14 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 arr1 = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
-arr1.sort()
-arr2.sort()
+
+# 방법1
+# arr1.extend(arr2)
+# arr1.sort()
+# print(*arr1)
+
+
+# 방법2 : 투포인터
 arr1_left = 0
 arr2_left = 0
 #new = []
@@ -18,11 +27,15 @@ while arr1_left < n and arr2_left < m:
     if a1 < a2:
         #new.append(a1)
         print(a1, end=" ")
-        if arr1_left < n: arr1_left+=1
+        arr1_left+=1
     elif a2 < a1:
         #new.append(a2)
         print(a2, end=" ")
-        if arr2_left < m: arr2_left+=1
+        arr2_left+=1
+    else:
+        print(a1, a2, end=" ")
+        arr1_left+=1
+        arr2_left+=1
 
 if arr1_left < n:
     print(*arr1[arr1_left:])
